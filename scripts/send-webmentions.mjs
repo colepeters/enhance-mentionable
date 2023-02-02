890a7198ee05ebd27826da65f1d1e84c1a039e33
Cole Peters <cole@colepeters.com>
2023-02-02 12:31:46 -0600

    Updates from Taylor

--- scripts/send-webmentions.mjs
+++ scripts/send-webmentions.mjs
@@ -1,23 +1,32 @@
-const WEBMENTION_ENDPOINT = 'https://making-2c4.begin.app/webmention'
+const myContent = 'https://nature-6o8.begin.app/'
 
-const sampleWebMention = {
-  source: 'https://nature-6o8.begin.app/', // your app's content URL
-  target: 'https://making-2c4.begin.app/', // the article you mention
+const mentions = {
+  'https://making-2c4.begin.app/webmention': { // Taylor
+    source: myContent,
+    target: 'https://making-2c4.begin.app/',
+  },
+  'https://tbeseda.com/webmention': { // tbeseda.com test article
+    source: myContent,
+    target: 'https://tbeseda.com/articles/2023/01/webmention-test',
+  },
 }
 
-const response = await fetch(WEBMENTION_ENDPOINT, {
-  method: 'POST',
-  body: new URLSearchParams(sampleWebMention),
-})
+for (const [endpoint, webmention] of Object.entries(mentions)) {
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    body: new URLSearchParams(webmention),
+  })
 
-let message
-if (response.ok) {
-  const contentType = response.headers.get('content-type')
-  message = contentType?.startsWith('application/json')
-    ? await response.json()
-    : await response.text()
-} else {
-  message = `Error ${response.status}: ${response.statusText}`
+  let message
+  if (response.ok) {
+    const contentType = response.headers.get('content-type')
+    message = contentType?.startsWith('application/json')
+      ? await response.json()
+      : await response.text()
+  } else {
+    message = `Error ${response.status}: ${response.statusText}`
+  }
+
+  console.log(message)
 }
 
-console.log(message)
